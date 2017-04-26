5d092a94996a8d380f13b6d26f44beb072f937ef
DANIEL LEE <thanhlq@topica.edu.vn>
2017-04-26 15:50:03 +0700

    chuc nang dat hang

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,7 +1,7 @@
 class LineItem < ApplicationRecord
 	belongs_to :product
 	belongs_to :cart
-
+	belongs_to :order
 	def line_total_price
 		product.price * quantity
 	end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :orders
   get 'admin/index'
 
   get 'sessions/new'
